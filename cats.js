f37344c2310c1464e52424aaae4bc365e38fabdf
NovMarina <marina.novikova1998@gmail.com>
2025-05-21 22:52:06 +0300

    first commit

---
+++ cats.js
@@ -0,0 +1,22 @@
+const smallImgs = document.querySelectorAll(".small_gallery img");
+const mainImg = document.querySelector("#main_Img");
+const info = document.querySelector(".right_info");
+
+function setMainImg(src, text) {
+    const newImg = mainImg.src
+    mainImg.src = src;
+    info.innerHTML = text;
+    return newImg;
+
+}
+
+for (let i = 0; i < smallImgs.length; i++) {
+    smallImgs[i].addEventListener("click", function () {
+
+        smallImgs[i].src = setMainImg(smallImgs[i].src, smallImgs[i].getAttribute('data-text'));
+    })
+
+}
+mainImg.ondblclick = function () {
+    mainImg.src = "./assers/cat_1.jpg";
+}
